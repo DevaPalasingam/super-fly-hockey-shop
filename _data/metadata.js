b45ca0b46c793511948490a1a8baa24a465a53ef
Deva <deva.palasingam@gmail.com>
2023-09-05 10:08:16 -0500

    initial setup

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Eleventy Base Blog v8",
+	url: "https://example.com/",
+	language: "en",
+	description: "This is a test blog",
+	author: {
+		name: "Your Name Here",
+		email: "youremailaddress@example.com",
+		url: "https://example.com/about-me/",
+	},
+};
